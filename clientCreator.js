fad7c6733be1bbc257af2fe1291abc93b64925f2
andydmort <andydmort@gmail.com>
2018-11-16 19:59:45 -0700

    Now there is a simple template render.

---
+++ clientCreator.js
@@ -0,0 +1,28 @@
+var fs = require('fs');
+
+function getPage(path){
+    return new Promise(function(resolve,reject){
+        fs.readFile(path,'utf8', function(err, data) {
+            if(err){
+                console.log("here1");
+                console.log("File Read recieved an error: "+err);
+            }
+            else{
+                resolve(data);
+            }
+        });
+    })
+}
+
+//patter Obj ex. {name: 'Johnathan'}
+function replaceInString(patternObj,fullString){
+    for( i in patternObj){
+        console.log("here: "+i);
+        pat = '\/\$'+ i +'\$\/'; //This is for the pattern /$ <Your pattern> $/ 
+        console.log(pat);
+        fullString = fullString.replace(pat,patternObj[i]);
+    }
+    return fullString;
+}
+
+module.exports = {'getPage':getPage, 'replaceInString':replaceInString};
